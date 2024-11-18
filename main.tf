4e3327baaf583090e25cb8832c87e8704543f8df
Yassine-Teimi <yassine.teimi@ibm.com>
2024-11-18 15:09:32 +0100

    Testing variable specific to a terraform run in HCP Terraform

--- main.tf
+++ main.tf
@@ -5,3 +5,25 @@ provider "aws" {
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+variable "instance_type" {
+  description = "The type of instance to deploy"
+  type        = string
+  default     = "t2.micro"  # Default for other environments
+}
+
+variable "feature_flag" {
+  description = "Enable experimental features"
+  type        = bool
+  default     = false
+}
+
+resource "aws_instance" "app" {
+  ami           = "ami-0acc77abdfc7ed5a6"
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = "staging-app"
+    FeatureFlag = var.feature_flag ? "enabled" : "disabled"
+  }
+}
